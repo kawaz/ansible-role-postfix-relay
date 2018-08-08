ced48dc00723355b0b478b269140adf9b7dfd057
Yoshiaki Kawazu <kawazzz@gmail.com>
2018-08-09 05:34:28 +0900

    Fix Lint
    ERROR [role] ./filter_plugins/to_smtp_credentials.py:2:1: F401 'ansible.errors' imported but unused
    ERROR [role] ./filter_plugins/to_smtp_credentials.py:7:1: E302 expected 2 blank lines, found 1
    ERROR [role] ./filter_plugins/to_smtp_credentials.py:10:1: E302 expected 2 blank lines, found 1
    ERROR Lint failed

--- filter_plugins/to_smtp_credentials.py
+++ filter_plugins/to_smtp_credentials.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
-from ansible import errors
 import base64
 import hashlib
 import hmac
-
 def to_smtp_credentials(aws_secret):
     return base64.b64encode(chr(2) + hmac.new(str(aws_secret), "SendRawEmail", hashlib.sha256).digest())
-
 class FilterModule(object):
     def filters(self):
         return {
